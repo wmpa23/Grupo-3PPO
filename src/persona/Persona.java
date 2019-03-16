d22f491dedfc71f43aa61a326fee4b49400d477c
Administrator <Administrator@NHPC105.inhworld.com>
2019-03-16 09:34:09 -0600

    Comparto mi parte de la Sub clase Estudiante.

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -18,13 +18,13 @@ public class Persona {
     public static void main(String[] args) {
         // TODO code application logic here
    Estudiante estudiante = new Estudiante();
-   Depostista depostista = new Depostista();
+ //  Depostista depostista = new Depostista();
  
     
   // Imprimi informacion desde Datos2
  System.out.println("El Nombre del Estudiante es: " + estudiante.getnombre());
  System.out.println("La Edad del Estudiantes es:" + estudiante.getedad()); 
- System.out.println("El deporte de Practica es: " + depostista.getperfil()); 
+ //System.out.println("El deporte de Practica es: " + depostista.getperfil()); 
     }
     }
     
