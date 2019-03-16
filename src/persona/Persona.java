ad933051fe8ca6be2c14a7f745c6c1a03bcbfda7
wpena23 <wpena@NHPC127.inhworld.com>
2019-03-16 09:01:09 -0600

    Primer Commit: Completando Clase Principal

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -12,23 +12,20 @@ import Datos.Estudiante;
  * @author wpena
  */
 public class Persona {
-
-    /**
+  /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
    Estudiante estudiante = new Estudiante();
-  // Deportista deportista = new Desportista();  
-    
-    //Imprimi datos Universidad
- //System.out.println(estudiante.getnombre());
- //System.out.println(estudiante.getedad()); 
- //System.out.println(estudiante.getperfil()); 
-     
+   Depostista depostista = new Depostista();
  
-     
+    
+  // Imprimi informacion desde Datos2
+ System.out.println("El Nombre del Estudiante es: " + estudiante.getnombre());
+ System.out.println("La Edad del Estudiantes es:" + estudiante.getedad()); 
+ System.out.println("El deporte de Practica es: " + depostista.getperfil()); 
+    }
     }
     
 }
-
