2fe665cccaed286049af09dac8fe2f0742cfb948
Administrator <Administrator@NHPC105.inhworld.com>
2019-03-16 09:37:06 -0600

    Comparto mi parte de la Sub clase Estudiante.

--- src/Datos/Estudiante.java
+++ src/Datos/Estudiante.java
@@ -9,67 +9,26 @@ package Datos;
  *
  * @author Administrator
  */
-public class Estudiante extends Datos {
 
-    public static void imprirnombre() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+public class Estudiante extends Datos{
+//abstractos
 
-  //Nombre
-    public String nombre;
-    //Apellido
-    private String perfil; //test
+ @Override
+ public String getnombre() {
+return "Mario";
+  }
+ @Override
+ public  String getedad() {
+ return "22";
+ }
+  
+ @Override
+  String getperfil() {
+    throw new UnsupportedOperationException("Not supported yet.");
     
-    //Edad
-    private int edad;
-    
-    
-    public Estudiante(String nombre, String perfil){
-        this.nombre = nombre;
-        this.perfil = perfil;
-    }
+ }
+}
 
-    public Estudiante() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    //Definir clase public para visualizar el nombre
-    public void imprimirnombre(){
-        System.out.println(nombre);
-    }
-    
-    public void imprimirperfil(){
-        System.out.println(perfil);
-    }
-    //Establecer valor de una clase public a privado
-    public void setedad(int _edad){
-        this.edad = _edad;
-    }
-    //Retornar valor de una clase public a privado
-    public int getedad(){
-        return edad;
-    }
-    //Establecer valor de una clase public a privado
-    public void seteexperiencia(int _experiencia){
-        this.experiencia = _experiencia;
-    }
-    //Retornar valor de una clase public a privado
-    public int getexperiencia(){
-        return experiencia;
-    }
-
-    @Override
-    String getnombre() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    String getperfil() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void imprimirdatos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
    
    
-} 
+
