075f17053d7848af114b85107ca71abdabbc1b1f
wmpa23 <48531189+wmpa23@users.noreply.github.com>
2019-03-17 13:40:54 -0600

    Update Persona.java

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -18,12 +18,14 @@ public class Persona {
     public static void main(String[] args) {
         // TODO code application logic here
    Estudiante estudiante = new Estudiante();
-   Depostista depostista = new Depostista();
+   Deportista deportista = new Deportista("Heydi", "Voleibol", 23);
  
     
   // Imprimi informacion desde Datos2
  System.out.println("El Nombre del Estudiante es: " + estudiante.getnombre());
  System.out.println("La Edad del Estudiantes es:" + estudiante.getedad()); 
- System.out.println("El deporte de Practica es: " + depostista.getperfil()); 
+ System.out.println("El Nombre del Deportista es: " + deportista.getnombre());
+ System.out.println("La edad del Deportista es: " + deportista.getedad());
+ System.out.println("El deporte que Practica es: " + deportista.getDeporte()); 
     }
 }
